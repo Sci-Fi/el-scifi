165480503acd55b267cd984134e0dabf8acbf265
CosmeFC <cosmefc@gmail.com>
2013-10-24 10:38:32 -0400

    Fix radius. Manager is used again.

--- modules/RADIUS.sh
+++ modules/RADIUS.sh
@@ -39,11 +39,12 @@ ln -s /etc/raddb /etc/freeradius
 
 #3
 rm /etc/raddb/modules/ldap /etc/raddb/sites-available/default /etc/raddb/clients.conf /etc/raddb/radiusd.conf /etc/raddb/eap.conf /etc/raddb/sites-available/inner-tunnel
-cp -rp $ModDir/RADIUS/raddb /etc/raddb/
+cp -rp $ModDir/RADIUS/raddb /etc/
 
 #4
 sed -i s/RADIUSACCOUNT/$RADIUSACCOUNT/g /etc/raddb/modules/ldap
 sed -i s/RADIUSACCPASS/$RADIUSACCPASS/g /etc/raddb/modules/ldap
+sed -i s/LDAPADMPASSWD/$LDAPADMPASSWD/g /etc/raddb/modules/ldap
 sed -i s/LDAPSERVER/$LDAPSERVER/g /etc/raddb/modules/ldap
 sed -i s/LDAPSUFIX/$LDAPSUFIX/g /etc/raddb/modules/ldap
 sed -i s/RADIUSPASS/$RADIUSPASS/g /etc/raddb/clients.conf
@@ -53,5 +54,5 @@ chkconfig radiusd on
 service radiusd restart
 
 echo RADIUS module finished
-echo Press <Enter> to exit
+echo 'Press <Enter> to exit'
 read
